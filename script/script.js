8fa87619fce68a23d62f0cd112d1a3eb3fbe44b7
Clarence Scott <111468756+clarencescott@users.noreply.github.com>
2024-02-23 14:05:58 -0500

    Full site upload for testing
    Full site upload for testing

---
+++ script/script.js
@@ -0,0 +1,23 @@
+let darkMode = true;
+
+function mode(){
+if (darkMode){
+    document.body.style.background = "linear-gradient(to bottom right, #39c467, rgb(226, 186, 55))";
+    document.getElementById("header").style.color = "black";
+    document.getElementById("footer").style.color = "black";
+    document.getElementById("footer").style.backgroundColor = "#39c467"; 
+    document.querySelectorAll('a').forEach(anchor =>{
+        anchor.style.color = "black";
+    })
+    darkMode = false;
+}
+else{
+    document.body.style.background = "linear-gradient(to bottom right, #080052, rgb(73, 0, 107))";
+    document.getElementById("header").style.color = "lightgrey";
+    document.getElementById("footer").style.color = "lightgrey";
+    document.getElementById("footer").style.backgroundColor = "#090058"; 
+    document.querySelectorAll('a').forEach(anchor =>{
+        anchor.style.color = "lightgrey";
+    })
+    darkMode = true;
+}} 
---
+++ script/toggle.js
@@ -0,0 +1,7 @@
+function contact(){ 
+    if (document.getElementById("contact").style.display === "block"){
+        document.getElementById("contact").style.display = "none";
+}else{
+        document.getElementById("contact").style.display = "block";
+}
+}
