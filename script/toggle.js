4233914cb5fcdbedf2ea4dfb50576655ed70204f
hqhealthcare <scottclarence5@gmail.com>
2024-04-02 18:35:27 -0400

    Removed old files

--- script/toggle.js
+++
@@ -1,7 +0,0 @@
-function contact(){ 
-    if (document.getElementById("contact").style.display === "block"){
-        document.getElementById("contact").style.display = "none";
-}else{
-        document.getElementById("contact").style.display = "block";
-}
-}
